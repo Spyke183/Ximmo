14406e9497376de03c6d498de4fca449d1b1f85a
Spyke183 <tinorompaul@gmail.com>
2023-11-21 11:20:29 +0100

    Navbae sans menu burger

--- src/components/Navbarfolder/Navbar.jsx
+++ src/components/Navbarfolder/Navbar.jsx
@@ -4,10 +4,15 @@ import "../Navbarfolder/Navbar.css";
 function Navbar() {
   return (
     <>
-      <div class="navbar">
-        <div class="logo">XIMMO</div>
-        <div class="menu">
-          <div class="btn-annonce"> Déposer une annonce</div>
+      <div class="contenu_navbar">
+        <div class="navbar">
+          <div class="logo">XIMMO</div>
+          <div class="menu">
+            <div class="btn-annonce"> Déposer une annonce</div>
+            <a href="#">Nous contacter</a>
+            <a href="#">Mes annonces (offres)</a>
+            <a href="#">Se déconnecter</a>
+          </div>
         </div>
       </div>
     </>
