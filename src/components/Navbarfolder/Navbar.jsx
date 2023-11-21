1495a877181d9a9710d4aa3d61c3ff9874121c37
Spyke183 <tinorompaul@gmail.com>
2023-11-21 10:43:53 +0100

    Création+mise en page css de la navbar

---
+++ src/components/Navbarfolder/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../Navbarfolder/Navbar.css";
+
+function Navbar() {
+  return (
+    <>
+      <div class="navbar">
+        <div class="logo">XIMMO</div>
+        <div class="menu">
+          <div class="btn-annonce"> Déposer une annonce</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
