4bfa8061a2901bd6a073952a7f58575d80c623df
Spyke183 <tinorompaul@gmail.com>
2023-11-22 15:39:48 +0100

    création du header pour acceuil

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-import Acceuil from "./router/Acceuil.jsx";
+import Acceuil from "./router/Acceuil/Acceuil.jsx";
 import ErrorPage from "./error-page";
 import Connexion from "./router/Connexion.jsx";
 import Inscription from "./router/Inscription.jsx";
@@ -16,15 +16,15 @@ const router = createBrowserRouter([
     element: <Acceuil />,
     errorElement: <ErrorPage />,
   },
+  {
+    path: "/",
+    element: <Acceuil />,
+    errorElement: <ErrorPage />,
+  },
 ]);
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
     <RouterProvider router={router} />
-    <Connexion />
-    {/*   <Inscription />*/}
-
-    <Annonce />
-    <Profil />
   </React.StrictMode>
 );
