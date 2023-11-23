74f0e395f4ef78f95449be40e69c5905f286e636
Spyke183 <114151896+Spyke183@users.noreply.github.com>
2023-11-23 23:41:15 +0100

    Deco/Co Navbar

--- src/components/Navbarfolder/Navbar.jsx
+++ src/components/Navbarfolder/Navbar.jsx
@@ -1,21 +1,35 @@
 import React from "react";
+import LogoutCompte from "../DecoCompte/LogoutCompte";
 import "../Navbarfolder/Navbar.css";
 
 function Navbar() {
+  const isUserLoggedIn = !!localStorage.getItem("token");
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    window.location.reload();
+  };
+
   return (
     <>
       <div className="contenu_navbar">
         <div className="navbar">
           <div className="logo">XIMMO</div>
           <div className="menu">
-            <div className="btn-annonce"> Déposer une annonce</div>
             <a href="/">Accueil</a>
-            <a href="/profil">Mes annonces (offres)</a>
-            <a href="#">Se déconnecter</a>
-            <a href="#">Se connecter</a>
+            {isUserLoggedIn && <a href="/profil">Mes annonces (offres)</a>}
+            {isUserLoggedIn ? (
+              <>
+                <a href="#" onClick={handleLogout}>Se déconnecter</a>
+              </>
+            ) : (
+              <>
+                <a href="/connexion">Se connecter</a>
+              </>
+            )}
           </div>
         </div>
-      </div>
+      </div> 
     </>
   );
 }
