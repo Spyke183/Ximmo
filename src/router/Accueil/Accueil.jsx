babdad797b499b090e09e4136e4ddaf845de838a
Spyke183 <tinorompaul@gmail.com>
2023-11-24 12:36:01 +0100

    slice descrip. update

--- src/router/Accueil/Accueil.jsx
+++ src/router/Accueil/Accueil.jsx
@@ -116,8 +116,8 @@ const Root = () => {
             <p>Prix: {property.price}</p>
             <p>Email: {property.email}</p>
             Description:{" "}
-            {property.description.length > 30
-              ? `${property.description.slice(0, 30)}...`
+            {property.description.length > 40
+              ? `${property.description.slice(0, 40)}...`
               : property.description}
             <p>Localisation: {property.location}</p>
           </div>
